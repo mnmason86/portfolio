e17629f3152a797a8968c76884d53e9e500dd11e
Mandy Mason <mnmason86@gmail.com>
2022-07-16 18:14:59 -0400

    removed unnecessary secondary language code

--- src/App.js
+++ src/App.js
@@ -19,7 +19,6 @@ class App extends Component {
   }
 
   applyPickedLanguage = (pickedLanguage, oppositeLangIconId) => {
-    // this.swapCurrentlyActiveLanguage(oppositeLangIconId);
     document.documentElement.lang = pickedLanguage;
     var resumePath =
       document.documentElement.lang === window.$primaryLanguage
@@ -28,24 +27,11 @@ class App extends Component {
     this.loadResumeFromPath(resumePath);
   }
 
-  // swapCurrentlyActiveLanguage = (oppositeLangIconId) => {
-  //   var pickedLangIconId =
-  //     oppositeLangIconId === window.$primaryLanguageIconId
-  //       ? window.$secondaryLanguageIconId
-  //       : window.$primaryLanguageIconId;
-  //   document
-  //     .getElementById(oppositeLangIconId)
-  //     .removeAttribute("filter", "brightness(40%)");
-  //   document
-  //     .getElementById(pickedLangIconId)
-  //     .setAttribute("filter", "brightness(40%)");
-  // }
-
   componentDidMount = () => {
     this.loadSharedData();
     this.applyPickedLanguage(
       window.$primaryLanguage,
-      // window.$secondaryLanguageIconId
+     
     );
   }
 
